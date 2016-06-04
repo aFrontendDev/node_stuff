a00a715680f768bb869c7ae689e74fda2143e519
ablackledge <a.blackledge@building-blocks.com>
2016-06-04 11:53:38 +0100

    tidying up server js, next step is to split it out

--- server.js
+++ server.js
@@ -1,67 +1,38 @@
-var http = require('http');
 var express = require('express');
-//var app = express();
-var fs = require('fs');
-var formidable = require('formidable');
-var util = require('util');
-var nano = require('nano')('http://127.0.0.1:5984/');
-var dispatcher = require('httpdispatcher');
-
-console.log('1');
-
-var server = http.createServer(function(request, res) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    getData(res);
-
-    //dispatcher.dispatch(request, response);
-
-    // response.writeHead(200, {'Content-Type': 'text/plain'});
-    // response.end('Index');
-
-    // if (request.method.toLowerCase() === 'get') {
-    // displayForm(response);
-    // } else if (request.method.toLowerCase() === 'post') {
-    // processAllFieldsOfTheForm(request, response);
-    // }
-
-}).listen(4562);
+var app = express();
+var fs = require('fs'); // reads html file so we can serve up a page, will likely change this
+var formidable = require('formidable'); // process form
+var nano = require('nano')('http://127.0.0.1:5984/'); // couchDB url
 
 
-// app.all('*', function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
+// START *** Settings headers to allow cross domain requests
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+// END *****
 
-// app.get('/test', function (request, response) {
-//     response.send('hello world');
-//     console.log('express details');
-//     getData(response);
-// });
 
-// app.listen(4562, function () {
-//     console.log('Example app listening on port 3000!');
-// });
+// START *** Express deals with urls
+app.get('/getData', function (request, response) {
+    getData(response);
+});
 
-// dispatcher.onGet("/page1", function(request, response) {
-//     // res.writeHead(200, {'Content-Type': 'text/plain'});
-//     // res.end('Page One');
-//     getData(response);
-// });
+app.get('/test', function (request, response) {
+    response.send('Testing');
+});
+// END *****
 
-// console.log('2');
 
-// var serverRead = http.createServer(function(request, response) {
-//     getData(response);
-// }).listen(8081);
+// START *** Use Express to listen to port
+app.listen(4562, function () {
+    console.log('Example app listening!');
+});
+// END *****
 
+// Get and return data from couchDB view - used as a webservice
 function getData(response) {
 
     var db = nano.db.use('angular_app');
@@ -69,38 +40,28 @@ function getData(response) {
     var rows = null;
     var dataArray = [];
 
-    console.log('2.1');
-
     db.view('tests', 'tests', {'key': type, 'include_docs': true}, function(err, body) {
         if (!err) {
-            console.log('2.2');
             rows = body.rows;
             rows.forEach(function(row) {
-            dataArray.push(row.doc);
-          });
+                dataArray.push(row.doc);
+              });
+            console.log('Got data :)');
         } else {
           console.log('noooooo');
         }
-
-        console.log('2.3');
-        console.log(dataArray);
 
         response.setHeader('Content-Type', 'application/json');
         response.writeHead(200, {
             'content-type': 'application/json'
         });
-        // response.write('showing data:\n\n');
-        // response.end(util.inspect({
-        //     rows: dataArray
-        // }));
         response.end(JSON.stringify(dataArray));
-        //response.end(dataArray);
     });
 }
 
-console.log('3');
 
-
+// START *** The following functions are for get and displaying a form and then saving the form data
+// Get html file and display on page
 function displayForm(response) {
     fs.readFile('form.html', function (err, data) {
         response.writeHead(200, {
@@ -112,8 +73,7 @@ function displayForm(response) {
     });
 }
 
-console.log('4');
-
+// process the form when submitted
 function processAllFieldsOfTheForm(request, response) {
     var form = new formidable.IncomingForm();
 
@@ -124,15 +84,11 @@ function processAllFieldsOfTheForm(request, response) {
             'content-type': 'text/plain'
         });
         response.write('received the data:\n\n');
-        response.end(util.inspect({
-            fields: fields,
-            files: files
-        }));
+        response.end();
     });
 }
 
-console.log('5');
-
+// Save the data from the form to couchDB
 function saveData(fields) {
     console.log('fields');
     console.log(fields);
@@ -150,11 +106,10 @@ function saveData(fields) {
     test_db.insert(data, function(err, body) {
         if (!err) {
             console.log('done :D');
-            //awesome
         } else {
           console.log('fail :(');
         }
     });
 }
+// END *****
 
-console.log('6');
